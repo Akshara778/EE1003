3e044af4ccec5541dba9a339ac9622a78f99263e
Akshara Sarma Chennubhatla <ee24btech11003@iith.ac.in>
2025-01-29 23:25:37 +0530

    eigth assignment

--- assignment-8/codes/ccode.c
+++ assignment-8/codes/ccode.c
@@ -4,15 +4,17 @@
 
 
 //function to generate the array of coordinates to be plotted
-void LU(int m, float A[m][m], float L[m][m]){
-	for(int i=0;i<
-}
-
-float newtonRalphson(float x, int n){
-	for (int i=0; i<(n);i++){
-    	x = x - ((x*x + 5*x - 1800) / (2*x + 5));
-  	}
-  	return x;
+float** pointsGenerate(float a, float b, float c, float x0, float h, int n){
+	float** points = malloc(sizeof(float*) * n);
+	float x = x0, y = -(c + (a * x))/b;
+	for(int i=0;i<n;i++){
+		points[i] = (float*)malloc(sizeof(float) * 2);
+		points[i][0] = x;
+		points[i][1] = y;
+		x += h;
+		y = -(c + (a * x))/b;
+	}
+	return points;
 }
 
 //function to free the array of coordinates after use
